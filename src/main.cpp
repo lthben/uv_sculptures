73ffeff295d22374e8aaa329faae92f15518591f
Benjamin Low <lthben@gmail.com>
2019-10-19 22:45:07 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+/*
+  Author: Benjamin Low (Lthben@gmail.com)
+  Date: Oct 2019
+  Description: Teensy 3.2 with audio shield. Sculpture 1 has 6 buttons. Sculpture 2 has 2 buttons and 2 sliders. Both have neon flex leds to show the UV levels in the form of light. There are sounds associated with all light animations, including a pulsing idle animation. Sculpture 1 shows UV levels in 6 diff places. Sculpture 2 has a playback mode for the buttons and sliders to show UV levels across time at a location. 
+*/
+
+//-------------------- USER DEFINED SETTINGS --------------------//
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  //test test
+
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
